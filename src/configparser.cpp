fcc00716da5e9fc06b4a755e20347db15faf8398
mksas <mathias.husted@live.dk>
2017-06-17 17:02:18 +0200

    Create configparser.cpp

---
+++ src/configparser.cpp
@@ -0,0 +1,36 @@
+//  Config file parser
+//  Mathias Husted 2017
+
+#include <iostream>
+#include <fstream>
+#include <map>
+#include "configparser.h"
+
+
+ConfigParser::ConfigParser(char* path) {
+	configPath = path;
+}
+
+std::string ConfigParser::ReturnPath() {
+	return configPath;
+}
+
+std::map<std::string, std::string> ConfigParser::Read() {
+	std::map<std::string, std::string> cfgStream;
+
+	std::ifstream cfgFile(configPath);
+
+	while (cfgFile) {
+		std::string key;
+		std::string value;
+		
+		std::getline(cfgFile, key, ':');
+		std::getline(cfgFile, value, '\n');
+
+		cfgStream[key] = value;
+	}
+
+	cfgFile.close();
+
+	return cfgStream;
+}
